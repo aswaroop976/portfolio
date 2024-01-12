6477bb4e31d71b7f948b662d94a03d3884e362d3
aswaroop976 <arpan.swaroop@gmail.com>
2024-01-12 17:10:15 -0600

    progress on my portfolio, can display portfolio projects and their technologies

---
+++ src/data/portfolio.js
@@ -0,0 +1,15 @@
+export default [
+    {
+        title: 'Unix-like 32 bit x86 operating system',
+        imgUrl: '',
+        stack: ['C', 'x86asm'],
+        link: '',
+    },
+    {
+        title: 'Chat-app',
+        imgUrl: '',
+        stack: ['GoLang', 'HTML', 'CSS', 'JavaScript'],
+        link: '', //host your website dumbass
+    },
+    //add your cs411 project here
+]
