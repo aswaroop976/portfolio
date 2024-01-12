4b7217868d9b09780d0c053f0031dacfb162f268
aswaroop976 <arpan.swaroop@gmail.com>
2024-01-12 14:37:58 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useState } from 'react'
+import Timeline from './components/timeline'
+import Portfolio from './components/portfolio'
+import Contact from './components/contact'
+import Footer from './components/footer'
+import Intro from './components/intro'
+
+function App() {
+  return (
+    <div className='App'>
+      <Intro />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </div>
+  )
+  // const [count, setCount] = useState(0)
+
+  // return (
+  //   <>
+  //     <div>
+  //       <a href="https://vitejs.dev" target="_blank">
+  //         <img src={viteLogo} className="logo" alt="Vite logo" />
+  //       </a>
+  //       <a href="https://react.dev" target="_blank">
+  //         <img src={reactLogo} className="logo react" alt="React logo" />
+  //       </a>
+  //     </div>
+  //     <h1>Vite + React</h1>
+  //     <div className="card">
+  //       <button onClick={() => setCount((count) => count + 1)}>
+  //         count is {count}
+  //       </button>
+  //       <p>
+  //         Edit <code>src/App.jsx</code> and save to test HMR
+  //       </p>
+  //     </div>
+  //     <p className="read-the-docs">
+  //       Click on the Vite and React logos to learn more
+  //     </p>
+  //   </>
+  // )
+}
+
+export default App
